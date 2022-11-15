305da92ad847b5be3a10e47a1412fe1eeed9831d
IsmeniaIMGO <113052394+IsmeniaIMGO@users.noreply.github.com>
2022-11-15 12:08:08 -0500

    clase principal

---
+++ src/hospital/Main.java
@@ -0,0 +1,15 @@
+package hospital;
+
+/**
+ * Clase principal
+ * @author Ismenia Marcela Guevara Ortiz
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Hospital hospital = new Hospital("La Sagrada Familia", 6); //se inicializa
+		hospital.simularPacientes();//se inicializan pacientes
+	}
+
+}
